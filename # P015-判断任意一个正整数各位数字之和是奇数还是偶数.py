91c450c24f1d903502580d74c13380c98ab435a2
PileCarrot <165862397+PileCarrot@users.noreply.github.com>
2024-04-03 11:10:32 +0800

    Update # P015-判断任意一个正整数各位数字之和是奇数还是偶数.py

--- # P015-判断任意一个正整数各位数字之和是奇数还是偶数.py
+++ # P015-判断任意一个正整数各位数字之和是奇数还是偶数.py
@@ -1,5 +1,6 @@
 # 编程判断任意一个正整数各位数字之和是奇数还是偶数。
 # 如果和是奇数输出1，偶数输出0。
 
-c=list(map(int,input()))#map函数强制转化
+c=list(map(int,input()))
+#map函数的基本语法为map(function, iterable, ...)，map() 函数将 function 应用于 iterable 中的每个元素并返回迭代后的结果
 print(sum(c)%2)#sum函数可以直接求list里面数据的和
