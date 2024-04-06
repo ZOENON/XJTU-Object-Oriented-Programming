997feefc6ba2f95b09a5c42543691ec230b8cac7
PileCarrot <165862397+PileCarrot@users.noreply.github.com>
2024-04-06 16:45:13 +0800

    Update # P017-删除串中的重复字符.py

--- # P017-删除串中的重复字符.py
+++ # P017-删除串中的重复字符.py
@@ -5,7 +5,7 @@
 # 输出：  删除重复字符后的字符串。例如：abced。
 
 
-
+'''
 #1.比较传统的C语言思路
 c=list(input())
 l=len(c)
@@ -21,7 +21,26 @@ while i<l:
     i+=1
 
 print(''.join(c))
+'''
 
+#以下方式已经经过检验
 
-#以下方式已经经过pycharm检验
+def remove_duplicates(input_str):
+    # 使用集合来存储已经出现过的字符
+    seen = set()
+    # 用于存储删除重复字符后的结果
+    result = []
 
+    for char in input_str:
+        if char not in seen:
+            # 如果字符不在集合中，将其添加到集合和结果中
+            seen.add(char)
+            result.append(char)
+
+    # 将结果列表转换为字符串并返回
+    return ''.join(result)
+
+# 测试示例
+input_str = input()
+output_str = remove_duplicates(input_str)
+print(output_str) 
