d4cc8587ef1b9fe78848559be92832997a828f12
PileCarrot <165862397+PileCarrot@users.noreply.github.com>
2024-04-06 15:31:17 +0800

    Update # P005-最大公约数.py

--- # P005-最大公约数.py
+++ # P005-最大公约数.py
@@ -9,14 +9,13 @@ a=c[0]
 for i in range(l-1):
     b=c[i+1]
     while b != 0:
-        t=a
-        a=b
-        b=t%b
-output=list(map(str,c))
-r=" ".join(output)
+        a,b=b,a%b
+
+# output=list(map(str,c))
+# r=" ".join(output)
 
 print(a)
-print(r)
+print(*c)
 #或者 print(a,'\n',r,sep="")
 #语法格式：print(*objects, sep=' ', end='\n', file=sys.stdout)，此处step设置为空，是为了去掉输出之间的空格（不然print默认空格作为分割））
 #用两个print的话a和r之间不需要加\n是因为print自带的end
