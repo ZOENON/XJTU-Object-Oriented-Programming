d45f3c26401c12c66737f7a63d1d224e27e6bc94
PileCarrot <165862397+PileCarrot@users.noreply.github.com>
2024-04-06 11:58:31 +0800

    Update # P034-和差积商.py

--- # P034-和差积商.py
+++ # P034-和差积商.py
@@ -1,7 +1,6 @@
 # 输入2个整数，求：和，差，积，商
 # 输入：2个整数（第二个数是非零整数）
 # 输出：4个整数，依次为和、差、积和商，数据间用空格分隔。
-a=list(map(float,input().split()))
-b=int(a[0])
-c=int(a[1])
-print(b+c,b-c,b*c,b/c)
+a,b=map(float,input().split())
+
+print(a+b,a-b,a*b,a/b)
