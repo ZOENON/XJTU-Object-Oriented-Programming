0694061554b8457bcf62bc98d01f251f445c65ea
ZOENON <songjiangcheng1101@gmail.com>
2024-05-17 22:36:31 +0800

    renew

--- # P029-闰年计算.py
+++ # P029-闰年计算.py
@@ -1,4 +1,4 @@
-def run(y)
+def run(y):
     if (y%4==0 and y%100!=0) or (y%400==0):
         return True
     else:
@@ -6,5 +6,9 @@ def run(y)
 y,n=map(int,input().split())
 while not run(y):
     y+=1
-for i in range(n):
-    if y
+for i in range(n-1):
+    if run(y+4):
+        y=y+4
+    else:
+        y=y+8
+print(y)
